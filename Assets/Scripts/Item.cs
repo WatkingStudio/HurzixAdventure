b8461a79cf5673920d6d49de326563b2f24a1b30
Gethin Watkins <watkingstudio@gmail.com>
2019-08-17 12:00:11 +0100

    Items Collectable

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-	[SerializeField] private ItemType m_ItemType;
+	[SerializeField] protected ItemType m_ItemType;
 
 	public enum ItemType
 	{

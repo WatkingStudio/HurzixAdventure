a65aba332121e89a705c2b8ec12347920676a9b2
Gethin Watkins <watkingstudio@gmail.com>
2019-10-22 07:02:13 +0100

    Lock Mechanic Works

--- Assets/LevelExit.cs
+++ Assets/LevelExit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelExit : InteractableObjects
 {
 	[SerializeField]
-	private List<GameObject> m_DoorLocks;
+	private List<LevelExitLock> m_DoorLocks;
 
 	private int m_NumberOfLocks;
 	private int m_LocksOpened = 0;
@@ -18,6 +18,15 @@ public class LevelExit : InteractableObjects
 	public override void Interact()
 	{
 		Debug.Log("Interact with Level Exit");
+
+		foreach (LevelExitLock exitLock in m_DoorLocks)
+		{
+			if (exitLock.Unlock())
+			{
+				m_LocksOpened++;
+				break;
+			}
+		}
 
 		if(m_LocksOpened == m_NumberOfLocks)
 		{

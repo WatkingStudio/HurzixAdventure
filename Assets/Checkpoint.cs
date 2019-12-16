bfe4155e5e1a50ae45cd0580c40998b97e8d4757
Gethin Watkins <watkingstudio@gmail.com>
2019-12-16 10:40:24 +0000

    Checkpoint Code

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Checkpoint : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.GetComponentInParent<PlayerController>())
+		{
+			if (!collision.GetComponentInParent<PlayerController>().IsPriorityCollider(collision))
+				return;
+		}
+		else
+			return;
+
+		collision.GetComponentInParent<PlayerCharacter>().UpdateCheckpoint(transform);
+	}
+
+	public Transform GetCheckpoint()
+	{
+		return transform;
+	}
+}

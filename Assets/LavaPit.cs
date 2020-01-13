7954427a5f4027229cdbb8add0a79b20b73099cc
Gethin Watkins <watkingstudio@gmail.com>
2020-01-13 20:27:20 +0000

    Lava Pits Added

---
+++ Assets/LavaPit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class LavaPit : MonoBehaviour
+{
+	[SerializeField]
+	private AudioSource m_AudioSource;
+
+    public void LavaCausesDamage()
+	{
+		if(m_AudioSource.clip != null && !m_AudioSource.isPlaying)
+			m_AudioSource.Play();
+	}
+}

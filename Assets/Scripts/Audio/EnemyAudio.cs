c321a8a629d7bc759fb75008605e03c511128fe7
Gethin Watkins <watkingstudio@gmail.com>
2021-03-23 15:54:36 +0000

    EnemyAudio Class Fixed

--- Assets/Scripts/Audio/EnemyAudio.cs
+++ Assets/Scripts/Audio/EnemyAudio.cs
@@ -21,12 +21,16 @@ public class EnemyAudio : MonoBehaviour
 	protected void Start()
 	{
 		if (!m_AudioSource)
+		{
 			Debug.LogError("No Audio source connected to " + gameObject.name);
-
+		}
 		if (m_WalkAudio.Count == 0)
+		{
 			Debug.LogError("No Walk Audio has been assigned to " + gameObject.name);
+		}
 	}
 
+	// Play the walking audio clip.
 	public void PlayWalkAudioClip()
 	{
 		if(!m_AudioSource.isPlaying)
@@ -36,6 +40,7 @@ public class EnemyAudio : MonoBehaviour
 		}
 	}
 
+	// Stop the audio clip.
 	public void StopAudioClip()
 	{
 		m_AudioSource.Stop();

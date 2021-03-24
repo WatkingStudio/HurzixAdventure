900498fa0734c5c9d81ec430ff2796d3b3fb6c69
Gethin Watkins <watkingstudio@gmail.com>
2021-03-24 08:49:43 +0000

    Snowman Classes Fixed

--- Assets/Scripts/Character/Enemy/Snowman/Snowman.cs
+++ Assets/Scripts/Character/Enemy/Snowman/Snowman.cs
@@ -17,6 +17,11 @@ public class Snowman : BasicEnemy
 	// Start is called before the first frame update
 	void Start()
     {
+		if(!m_EnemyPlayerDetection)
+        {
+			Debug.LogError("No EnemyPlayerDetection script has been assigned to " + gameObject.name);
+		}
+
 		SetActiveAction(m_DefaultAction);
 		m_StartPosition = gameObject.transform.position;
     }
@@ -25,31 +30,41 @@ public class Snowman : BasicEnemy
     void Update()
     {
 		if (m_ActiveAction == null)
+		{
 			return;
+		}
 		m_ActiveAction.PerformAction();
 
-		if(m_ActiveAction.Action == EnemyAction.Actions.EnemyRangedAttack)
-			if(!m_EnemyPlayerDetection.CanPlayerBeSeen())
+		if (m_ActiveAction.Action == EnemyAction.Actions.EnemyRangedAttack)
+		{
+			if (!m_EnemyPlayerDetection.CanPlayerBeSeen())
+			{
 				SetActiveAction(EnemyAction.Actions.EnemyPatrol);
+			}
+		}
     }
 
+	// Activate When a Player has been Detected.
 	public void PlayerDetected()
 	{
 		SetActiveAction(EnemyAction.Actions.EnemyRangedAttack);
 	}
 
+	// Reset the Enemy.
 	public override void ResetEnemy()
 	{
 		base.ResetEnemy();
 	}
 
-	public override void StopEnemy()
-	{
-		
-	}
-
+	// Set the if the Snowman is Walking or Not.
 	public override void SetWalking(bool walking)
 	{
 		base.SetWalking(walking);
+	}
+
+	// Stop the Enemy.
+	public override void StopEnemy()
+	{
+		
 	}
 }

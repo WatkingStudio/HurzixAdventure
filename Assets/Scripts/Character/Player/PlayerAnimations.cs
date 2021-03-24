353ab773dc6fd684a77f479854e6f2b806699e06
Gethin Watkins <watkingstudio@gmail.com>
2021-03-24 10:05:31 +0000

    Fixes Player Classes

--- Assets/Scripts/Character/Player/PlayerAnimations.cs
+++ Assets/Scripts/Character/Player/PlayerAnimations.cs
@@ -13,80 +13,100 @@ using UnityEngine;
 public class PlayerAnimations : MonoBehaviour
 {
 	[Header("Animator Controllers")]
-	[SerializeField, Tooltip("The animator for the player while standing")]
-	private RuntimeAnimatorController m_StandingController;
-	[SerializeField, Tooltip("The animator for the player while crouching")]
-	private RuntimeAnimatorController m_CrouchingController;
 	[SerializeField]
 	private Animator m_Animator;
+	[SerializeField, Tooltip("The animator for the player while crouching")]
+	private RuntimeAnimatorController m_CrouchingController;
+	[SerializeField, Tooltip("The animator for the player while standing")]
+	private RuntimeAnimatorController m_StandingController;
 
     // Start is called before the first frame update
     void Start()
     {
 		if (!m_StandingController)
+		{
 			Debug.LogError("No Animator Controller has been assigned to " + gameObject.name + " while standing");
+		}
 
 		if (!m_CrouchingController)
+		{
 			Debug.LogWarning("No Animator Controller has been assigned to " + gameObject.name + "while crouching");
+		}
 
 		if (!m_Animator)
+		{
 			Debug.LogError("No Animator has been assigned to " + gameObject.name);
+		}
 
 		m_Animator.runtimeAnimatorController = m_StandingController;
-    }
-
-	public void PlayerDead(bool isDead)
-	{
-		m_Animator.SetBool("IsDead", isDead);
 	}
 
-	public void PlayerHurt()
-	{
-		m_Animator.SetTrigger("Hurt");
-	}
-
-	public void PlayerIdle()
-	{
-		m_Animator.SetTrigger("Idle");
-	}
-
-	public void PlayerSprinting(bool isSprinting)
-	{
-		m_Animator.SetBool("IsSprinting", isSprinting);
-	}
-
+	// Tell the Animator to Perform an Attack Animation.
 	public void PlayerAttack()
 	{
 		m_Animator.SetTrigger("Attack");
 	}
 
-	public void PlayerJumping(bool isJumping)
-	{
-		m_Animator.SetBool("IsJumping", isJumping);
-	}
-
-	//When the player crouches/uncrouches, both of the animators need to know this. So before
-	// changing controller the bool is set to true/false, and after the controller is set
-	// the bool in the other controller is also set to true/false
+	// Tell the Animator to Crouch/Uncrouch.
 	public void PlayerCrouching(bool isCrouching)
 	{
+		//When the player crouches/uncrouches, both of the animators need to know this. So before
+		// changing controller the bool is set to true/false, and after the controller is set
+		// the bool in the other controller is also set to true/false.
+
 		m_Animator.SetBool("IsCrouching", isCrouching);
 
 		if (isCrouching)
+		{
 			m_Animator.runtimeAnimatorController = m_CrouchingController;
+		}
 		else
+		{
 			m_Animator.runtimeAnimatorController = m_StandingController;
+		}
 
 		m_Animator.SetBool("IsCrouching", isCrouching);
 	}
 
+	// Tell the Animator to Perform the Dead Animation.
+	public void PlayerDead(bool isDead)
+	{
+		m_Animator.SetBool("IsDead", isDead);
+	}
+
+	// Tell the Animator to Perform the Falling Animation.
 	public void PlayerFalling(bool isFalling)
 	{
 		m_Animator.SetBool("IsFalling", isFalling);
 	}
 
+	// Tell the Animator to Perform the Hurt Animation.
+	public void PlayerHurt()
+	{
+		m_Animator.SetTrigger("Hurt");
+	}
+
+	// Tell the Animator to Perform the Idle Animation.
+	public void PlayerIdle()
+	{
+		m_Animator.SetTrigger("Idle");
+	}
+
+	// Tell the Animator if the Player is Jumping.
+	public void PlayerJumping(bool isJumping)
+	{
+		m_Animator.SetBool("IsJumping", isJumping);
+	}
+
+	// Tells the Animator the Players Speed.
 	public void PlayerSpeed(float playerSpeed)
 	{
 		m_Animator.SetFloat("Speed", Mathf.Abs(playerSpeed));
+	}
+
+	// Tells the Animator if the Player is Sprinting.
+	public void PlayerSprinting(bool isSprinting)
+	{
+		m_Animator.SetBool("IsSprinting", isSprinting);
 	}
 }
